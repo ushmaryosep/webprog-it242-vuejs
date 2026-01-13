01c3da959a296168077ea770a39c8cad3152f1ef
Joshmar Clavero <jlclavero@student.apc.edu.ph>
2026-01-13 02:46:41 +0000

    Added component FoodItem

---
+++ firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Bananas',
+      message: 'I like bananas because it is full of potassium'
+    }
+  }
+};
+</script>
+
+<style></style>

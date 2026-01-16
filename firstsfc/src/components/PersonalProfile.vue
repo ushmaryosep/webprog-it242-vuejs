f9ad625ee094dd4f8340ab490aca18d2653ae562
Joshmar Clavero <jlclavero@student.apc.edu.ph>
2026-01-16 11:05:58 +0800

    tryyyyy

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,40 +1,41 @@
 <template>
-<!DOCTYPE html>
-<html>
-<head>
-  <title>My first Vue page</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <div class="grid-container">
 
-</head>
+    <div class="header">
+      <h1>My Personal Profile Web Page</h1>
+    </div>
 
-<body>
+    <div class="content">
+      <h1>JOSHMAR CLAVERO</h1>
+      <p><b><i>CURRENTLY A SECOND YEAR STUDENT OF ASIA PACIFIC COLLEGE</i></b></p>
+      <p>
+        <b>Education / Achievements:</b><br>
+        STEM - HSL Braille College, Inc. 2024<br>
+        BSIT - Asia Pacific College 2028
+      </p>
+      <p><i>Currently learning a lot about web and mobile programming.</i></p>
+      <p><i>My hobbies are cars, motorcycles, movies, anime, and eating.</i></p>
+    </div>
 
-<div class="grid-container">
+    <div class="facts">
+      <h2>My Picture</h2>
+      <img
+        src="https://raw.githubusercontent.com/ushmaryosep/WEBPROG-IT242-HTML-Lessons/refs/heads/feature/css-flexbox/IMAGES%20OF%20ME.png"
+        alt="My Photo"
+      />
+    </div>
 
-  <div class="header">
-    <h1>My Personal Profile Web Page</h1>
+    <div class="footer">
+      <p><b>WEBPROG IT242</b></p>
+    </div>
+
   </div>
-
-  <div class="content">
-    <h1>JOSHMAR CLAVERO</h1>
-    <p><b><i>CURRENTLY A SECOND YEAR STUDENT OF ASIA PACIFIC COLLEGE</i></b></p>
-    <p><b>Education / Achievements:</b><br>
-       STEM - HSL Braille College, Inc. 2024<br>
-       BSIT - Asia Pacific College 2028
-    </p>
-    <p><i>Currently learning a lot about web and mobile programming.</i></p>
-    <p><i>My hobbies are cars, motorcycles, movies, anime, and eating.</i></p>
-  </div>
-
-  <div class="facts">
-    <h2>My Picture</h2>
-    <img src="https://raw.githubusercontent.com/ushmaryosep/WEBPROG-IT242-HTML-Lessons/refs/heads/feature/css-flexbox/IMAGES%20OF%20ME.png" alt="My Photo">
-  </div>
-
-  <div class="footer">
-    <p><b>WEBPROG IT242</b></p>
-  </div>
-</html>
 </template>
-<script></script>
+
+<script>
+export default {
+  name: "PersonalProfile"
+}
+</script>
+
 <style src="./css/personalprofile.css"></style>

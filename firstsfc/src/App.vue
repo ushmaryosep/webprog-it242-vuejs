e6555b43169c3fd46b568e78da0259bc916c014d
Joshmar Clavero <jlclavero@student.apc.edu.ph>
2026-01-16 10:47:34 +0800

    added personal profile v1

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>FAVORITE FOOD PLACES</h1>
+  <personal-profile/>
   <food-item/>
   <food-item2/>
 </template>
---
+++ firstsfc/src/components/PersonalProfile.vue
@@ -0,0 +1,28 @@
+<template>
+<link rel="stylesheet" href="components/css/personalprofile.css?version=0.1">
+<div class="grid-container">
+
+  <div class="header">
+    <h1>My Personal Profile Web Page</h1>
+  </div>
+
+  <div class="content">
+    <h1>JOSHMAR CLAVERO</h1>
+    <p><b><i>CURRENTLY A SECOND YEAR STUDENT OF ASIA PACIFIC COLLEGE</i></b></p>
+    <p><b>Education / Achievements:</b><br>
+       STEM - HSL Braille College, Inc. 2024<br>
+       BSIT - Asia Pacific College 2028
+    </p>
+    <p><i>Currently learning a lot about web and mobile programming.</i></p>
+    <p><i>My hobbies are cars, motorcycles, movies, anime, and eating.</i></p>
+  </div>
+
+  <div class="facts">
+    <h2>My Picture</h2>
+    <img src="https://raw.githubusercontent.com/ushmaryosep/WEBPROG-IT242-HTML-Lessons/refs/heads/feature/css-flexbox/IMAGES%20OF%20ME.png" alt="My Photo">
+  </div>
+
+  <div class="footer">
+    <p><b>WEBPROG IT242</b></p>
+  </div>
+</template>
